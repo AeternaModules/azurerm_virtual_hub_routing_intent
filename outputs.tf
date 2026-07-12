e3ee186130e60e0fb48081677340a2c7893e3ccc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "virtual_hub_routing_intents" {
-  description = "All virtual_hub_routing_intent resources"
-  value       = azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents
-}
 output "virtual_hub_routing_intents_name" {
-  description = "List of name values across all virtual_hub_routing_intents"
-  value       = [for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : v.name]
+  description = "Map of name values across all virtual_hub_routing_intents, keyed the same as var.virtual_hub_routing_intents"
+  value       = { for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : k => v.name }
 }
 output "virtual_hub_routing_intents_routing_policy" {
-  description = "List of routing_policy values across all virtual_hub_routing_intents"
-  value       = [for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : v.routing_policy]
+  description = "Map of routing_policy values across all virtual_hub_routing_intents, keyed the same as var.virtual_hub_routing_intents"
+  value       = { for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : k => v.routing_policy }
 }
 output "virtual_hub_routing_intents_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_routing_intents"
-  value       = [for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_routing_intents, keyed the same as var.virtual_hub_routing_intents"
+  value       = { for k, v in azurerm_virtual_hub_routing_intent.virtual_hub_routing_intents : k => v.virtual_hub_id }
 }
 
